da5be3ffbe58bdfce7e73114b58b1ab443b8b68a
Warleson candido da Silva <warleson.candido@rarolabs.com.br>
2020-10-21 16:04:02 -0300

    focando elemento

--- lib/main.dart
+++ lib/main.dart
@@ -38,33 +38,29 @@ class _MyHomePageState extends State<MyHomePage> {
     var size = MediaQuery.of(context).size;
     OverlayState overlayState = Overlay.of(context);
     OverlayEntry overlayEntry = OverlayEntry(builder: (context) {
-      return Stack(
-        children: [
-          Container(
-            width: size.width,
-            height: size.height,
-            color: Colors.transparent,
-          ),
-          Positioned(
-              top: top,
-              left: left,
-              child: Container(
-                width: width,
-                height: height,
-                decoration: BoxDecoration(
-                    color: Colors.transparent,
-                    border: Border.all(color: Colors.black)),
-              )),
-        ],
+      return Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Stack(
+          children: [
+            CustomPaint(
+              size: size,
+              painter: HolePainter(
+                dx: left + 30,
+                dy: top + 30,
+                radius: width,
+              ),
+            ),
+            Center(
+              child: Text(
+                "Oi eu sou a Duda cabecuda",
+                style: TextStyle(color: Colors.white),
+              ),
+            )
+          ],
+        ),
       );
     });
-    OverlayEntry overlayEntry1 = OverlayEntry(builder: (context) {
-      return Container(
-        width: size.width,
-        height: size.height,
-        color: Colors.transparent,
-      );
-    });
+
     overlayState.insert(overlayEntry);
   }
 
@@ -84,26 +80,79 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        leading: Icon(
+          Icons.ac_unit,
+        ),
+        actions: [CircleAvatar()],
       ),
-      body: Center(
-        child: Container(
-          key: _key,
-          color: Colors.red,
-          width: 200,
-          height: 200,
+      body: Container(
+        child: Center(
+          child: Container(
+            height: 100,
+            width: 100,
+            color: Colors.red,
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
+        key: _key,
         onPressed: () async {
           await capturePositionWidget();
           showOverlay(context);
         },
-        tooltip: 'Increment',
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
+
+class HolePainter extends CustomPainter {
+  final double dx;
+  final double dy;
+  final double radius;
+  final ShapeFocus shapeFocus;
+
+  HolePainter({
+    this.dx,
+    this.dy,
+    this.radius,
+    this.shapeFocus = ShapeFocus.oval,
+  });
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()..color = Colors.black54;
+    if (shapeFocus == ShapeFocus.oval) {
+      canvas.drawPath(
+          Path.combine(
+            PathOperation.difference,
+            Path()..addRect(Rect.fromLTWH(0, 0, size.width, size.height)),
+            Path()
+              ..addOval(
+                  Rect.fromCircle(center: Offset(dx, dy), radius: radius - 30))
+              ..close(),
+          ),
+          paint);
+    } else {
+      canvas.drawPath(
+          Path.combine(
+            PathOperation.difference,
+            Path()..addRect(Rect.fromLTWH(0, 0, size.width, size.height)),
+            Path()
+              ..addRect(
+                  Rect.fromCircle(center: Offset(dx, dy), radius: radius - 30))
+              ..close(),
+          ),
+          paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
+  }
+}
+
+enum ShapeFocus { oval, square }
